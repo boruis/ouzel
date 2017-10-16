996d7ad467424df544aa73c64c2943af4ea2f164
yanweicai <yanweicai@moqihuyu.com>
2017-10-16 18:06:01 +0800

    lua code works only if disable OUZEL_SUPPORTS_SSE

--- samples/Resources/test.lua
+++ samples/Resources/test.lua
@@ -1,15 +1,25 @@
-local newScene = oz.Scene.new()
 local sceneManager = oz.sharedEngine:getSceneManager()
 
+local newScene = oz.Scene.new()
 
 local layer = oz.Layer.new()
 newScene:addLayer(layer)
 
-
 sceneManager:setScene(newScene)
 
-
-
+-- add camera
 local camera = oz.Camera.new(oz.Size2.new(400, 600), 3)
 layer:addChild(camera)
 dump(camera)
+
+
+-- display a sprite
+local testActor = oz.Actor.new()
+layer:addChild(testActor)
+
+
+local characterSprite = oz.Sprite.new()
+characterSprite:init("run.json", false, 0, 0, oz.Vector2.new(0.5, 0.5))
+characterSprite:play(true, 0.1)
+
+testActor:addComponent(characterSprite)
